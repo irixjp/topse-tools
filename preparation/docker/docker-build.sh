68faa086c99e9f106d48a2ec78c23a888418eca4
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2016-12-21 13:16:25 +0900

    test

--- preparation/docker/docker-build.sh
+++ preparation/docker/docker-build.sh
@@ -1,3 +1,22 @@
+# prepare
+yum install -y docker
+
+## NIIサーバーは /mnt に大容量領域がマウントされるため（変更不可）
+mv /var/lib/docker /mnt
+ln -s /mnt/docker /var/lib/docker
+rm -Rf /var/lib/docker/*
+
+## 100GBのイメージまで作れるようにする（デフォ10GB）
+vi /etc/sysconfig/docker-storage
+-----
+DOCKER_STORAGE_OPTIONS="-g /mnt/docker --storage-opt=dm.basesize=100G"
+-----
+
+systemctl enable docker
+systemctl start  docker
+
+
+# build
 docker build -t irixjp/topse-cloud-repo:mitaka-v1.2 .
 docker tag docker.io/jenkins irixjp/topse-cloud-jenkins2-master:mitaka-v1.0
 docker build --build-arg reposerver_ip=172.17.0.2 -t irixjp/topse-cloud-jenkins2-slave:mitaka-v1.0 .
--- preparation/docker/docker-run.sh
+++ preparation/docker/docker-run.sh
@@ -1,3 +1,10 @@
-docker run -d -p 80:80 --name repo irixjp/topse-cloud-repo:mitaka-v1.2
+# run
+docker run -d -p 80:80 \
+       --name repo \
+       -v /mnt/topse-tools/hands-on:/var/www/html/openstack/tools \
+       -v /mnt/topse-tools/preparation:/var/www/html/openstack/prepare \
+       irixjp/topse-cloud-repo:mitaka-v1.2
+
 docker run -d -p 8080:8080 -p 50000:50000 --name jenkins-master irixjp/topse-cloud-jenkins2-master:mitaka-v1.0
+
 docker run -d -p 10022:22 --name jenkins-slave  irixjp/topse-cloud-jenkins2-slave:mitaka-v1.0
