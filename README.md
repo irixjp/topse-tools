4e4c413b474ddd83fd6e1ec66cc531e5c5ebd510
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2016-12-21 23:27:13 +0900

    test

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# topse-tools
+# Some materials of Cloud infrastructure design and deployment course
+
+# Requirement
+
+必要パッケージ
+```
+yum install -y epel-release
+yum install -y qemu-kvm libvirt virt-manager virt-install \
+               libguestfs libguestfs-tools \
+               docker-io \
+               screen tmux jq
+```
+
+ディレクトリの設定
+```
+mv /var/lib/docker /mnt
+ln -s /mnt/docker /var/lib/docker
+rm -Rf /var/lib/docker/*
+
+mv /var/lib/libvirt/images /mnt
+ln -s /mnt/images /var/lib/libvirt/images
+```
+
+100GBのイメージまで作れるようにする（デフォ10GB）
+```/etc/sysconfig/docker-storage
+DOCKER_STORAGE_OPTIONS="-g /mnt/docker --storage-opt=dm.basesize=100G"
+```
+
+```
+systemctl enable docker
+systemctl start  docker
+```
--- preparation/docker/docker-run.sh
+++ preparation/docker/docker-run.sh
@@ -1,10 +1,10 @@
 # run
 docker run -d -p 80:80 \
        --name repo \
-       -v /mnt/topse-tools/hands-on:/var/www/html/openstack/tools \
-       -v /mnt/topse-tools/preparation:/var/www/html/openstack/prepare \
-       irixjp/topse-cloud-repo:mitaka-v1.2
+       -v /mnt/topse-tools/hands-on:/var/www/html/hands-on \
+       -v /mnt/topse-tools/preparation:/var/www/html/preparation \
+       irixjp/topse-cloud-repo:mitaka-v1.4
 
 docker run -d -p 8080:8080 -p 50000:50000 --name jenkins-master irixjp/topse-cloud-jenkins2-master:mitaka-v1.0
 
-docker run -d -p 10022:22 --name jenkins-slave  irixjp/topse-cloud-jenkins2-slave:mitaka-v1.0
+docker run -d -p 22 --name jenkins-slave  irixjp/topse-cloud-jenkins2-slave:mitaka-v1.2
