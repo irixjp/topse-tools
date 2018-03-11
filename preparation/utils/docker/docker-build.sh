346e25d84a3cc3a3bd6521c40e93ca74d4e65c32
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2018-03-11 17:33:30 +0900

    renew dockerfiles for repo

--- preparation/utils/docker/docker-build.sh
+++
@@ -1,8 +0,0 @@
-# build
-docker build -t irixjp/topse-cloud-repo:mitaka-v1.4 --no-cache=true --force-rm=true --rm=true .
-
-docker build --build-arg reposerver_ip=172.17.0.2 -t irixjp/topse-cloud-jenkins2-slave:mitaka-v1.2 --no-cache=true --force-rm=true --rm=true .
-
-docker tag docker.io/jenkins irixjp/topse-cloud-jenkins2-master:mitaka-v1.0
-
-docker build --build-arg reposerver_ip=172.17.0.2 -t irixjp/topse-cloud-tempest:mitaka-v1.0 --no-cache=true --force-rm=true --rm=true .
